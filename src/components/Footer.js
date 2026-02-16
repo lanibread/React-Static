0ec56da946f72a8b637b198acda425cac8fd2f1e
lanibread <brendikali@gmail.com>
2026-02-16 15:16:17 -0500

    changed text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer() {
-    return <p>Moral: Dishonesty can lead to loss of trust and people will not believe you.</p>;
+    return <p>Moral: By facing life's challenges with an open heart, you can learn to overcome them.</p>;
 }
 
 export default Footer;

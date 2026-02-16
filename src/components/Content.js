e61c82c743583c6b5649e4b3f9fcb13d212da6f6
lanibread <brendikali@gmail.com>
2026-02-16 15:50:08 -0500

    added text

---
+++ src/components/Content.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Content() {
+    return (
+        <p>
+            A pilot crashes his plane and while trying to fix his engine, he meets a young boy ,'the little prince'.
+            He comes from a tiny planet in which a singular rose, which he loves but doesn't exactly trust, lives alongside him. 
+            The little prince, seeking an escape from his lonely planet, decides to visit six other neighboring ones.
+            Each planet had a single adult with a singular obsession, their inhabitants consisted of a king, a vain man, a drunkard, a businessman, a lamplighter, and a geographer.
+        
+            The little prince decides to arrive on Earth and explores the desert in which he found the pilot.
+            While exploring, he meets a snake, a three-petaled rose, a fox and eventually a rose garden.
+            The rose garden deepens his understanding of love while his new fox friend highlights the importance of forming bonds.
+            Learning that his love for his rose is what makes it so special, the little prince decides to let the snake bite him in order to return back to his planet.
+        </p>
+    );
+}
+
+export default Content;
